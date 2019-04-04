c5ae7227a38e1812a179045b1fe243f363c27386
Kyle Bai <k2r2.bai@gmail.com>
2019-04-04 15:06:56 +0800

    Make sure cleanup.sh will delete RBAC resources

--- helm/cleanup.sh
+++ helm/cleanup.sh
@@ -6,6 +6,9 @@ NAMESPACE=${2:-ibm-system}
 kubectl delete MutatingWebhookConfiguration image-admission-config --ignore-not-found=true
 kubectl delete ValidatingWebhookConfiguration image-admission-config --ignore-not-found=true
 
+kubectl delete clusterrolebinding admission-portieris-webhook --ignore-not-found=true
+kubectl delete clusterroles portieris --ignore-not-found=true --ignore-not-found=true
+
 kubectl delete crd clusterimagepolicies.securityenforcement.admission.cloud.ibm.com imagepolicies.securityenforcement.admission.cloud.ibm.com --ignore-not-found=true
 
 kubectl delete jobs -n ${NAMESPACE} create-admission-webhooks create-armada-image-policies create-crds validate-crd-creation --ignore-not-found=true
