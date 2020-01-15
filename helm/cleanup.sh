0e50b432c0dc4eba9d14076264a69e4a9231f8ae
Stuart Hayton <stuart.hayton@uk.ibm.com>
2020-01-15 16:52:57 +0000

    dont delete permission

--- helm/cleanup.sh
+++ helm/cleanup.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 
-RELEASE_NAME=$1
+RELEASE_NAME=${1:-portieris}
 NAMESPACE=${2:-ibm-system}
+
+echo Purging release ${RELEASE_NAME} in ${NAMESPACE}
 
 kubectl delete MutatingWebhookConfiguration image-admission-config --ignore-not-found=true
 kubectl delete ValidatingWebhookConfiguration image-admission-config --ignore-not-found=true
-
-kubectl delete clusterrolebinding admission-portieris-webhook --ignore-not-found=true
-kubectl delete clusterroles portieris --ignore-not-found=true --ignore-not-found=true
 
 kubectl delete crd clusterimagepolicies.securityenforcement.admission.cloud.ibm.com imagepolicies.securityenforcement.admission.cloud.ibm.com --ignore-not-found=true
 
 kubectl delete jobs -n ${NAMESPACE} create-admission-webhooks create-armada-image-policies create-crds validate-crd-creation --ignore-not-found=true
 
-helm delete --purge $RELEASE_NAME
+helm delete --purge ${RELEASE_NAME}
 
 kubectl delete jobs -n ${NAMESPACE} delete-admission-webhooks delete-crds --ignore-not-found=true
