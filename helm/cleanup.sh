2a94dd728d656d5dbd4db611134fe2afdb3dacee
IsThisTheMatrix <46820823+causalityloop@users.noreply.github.com>
2020-07-22 16:58:39 -0400

    Update cleanup.sh

--- helm/cleanup.sh
+++ helm/cleanup.sh
@@ -1,16 +1,15 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 RELEASE_NAME=${1:-portieris}
 NAMESPACE=${2:-portieris}
 
-echo Deleting release ${RELEASE_NAME} in ${NAMESPACE}
+echo Deleting release "${RELEASE_NAME}" in "${NAMESPACE}"
 
 kubectl delete MutatingWebhookConfiguration image-admission-config --ignore-not-found=true
 kubectl delete ValidatingWebhookConfiguration image-admission-config --ignore-not-found=true
 
 kubectl delete crd clusterimagepolicies.securityenforcement.admission.cloud.ibm.com imagepolicies.securityenforcement.admission.cloud.ibm.com --ignore-not-found=true
 
-kubectl delete jobs -n ${NAMESPACE} create-admission-webhooks create-armada-image-policies create-crds validate-crd-creation --ignore-not-found=true
+kubectl delete jobs -n "${NAMESPACE}" create-admission-webhooks create-armada-image-policies create-crds validate-crd-creation --ignore-not-found=true
 
-helm delete ${RELEASE_NAME} --no-hooks 
-
+helm delete "${RELEASE_NAME}" --no-hooks --namespace "${NAMESPACE}"
