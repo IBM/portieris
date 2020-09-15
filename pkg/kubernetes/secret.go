e44be2dd9c6f75f0a290bfe6811afb8adfa5fe96
James Hart <jhart@uk.ibm.com>
2020-09-15 10:39:41 +0100

    Use registry credentials from encoded auth field if set

--- pkg/kubernetes/secret.go
+++ pkg/kubernetes/secret.go
@@ -15,8 +15,10 @@
 package kubernetes
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/golang/glog"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -27,8 +29,11 @@ type Auths struct {
 	Registries RegistriesStruct `json:"auths"`
 }
 
-// RegistriesStruct is a map of registries
-type RegistriesStruct map[string]struct {
+// RegistriesStruct is a map of registries to their credentials
+type RegistriesStruct map[string]RegistryCredentials
+
+// RegistryCredentials defines the fields stored per registry in an docker config secret
+type RegistryCredentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
@@ -84,13 +89,45 @@ func (w *Wrapper) GetSecretToken(namespace, secretName, registry string) (string
 	// Determine if there is a secret for the specified registry
 	registries := auths.Registries
 	if login, ok := registries[registry]; ok {
-		username = login.Username
-		password = login.Password
+		username, password = w.extractRegistryCredentials(login)
 	} else {
 		err = fmt.Errorf("Secret %s not defined for registry: %s", secretName, registry)
 	}
 	// glog.Infof("getSecretToken >> : token(%s)", token)
 	return username, password, err
+}
+
+func (w *Wrapper) extractRegistryCredentials(creds RegistryCredentials) (username, password string) {
+	username = creds.Username
+	password = creds.Password
+
+	if creds.Auth == "" {
+		return
+	}
+
+	decoder := base64.StdEncoding
+	if !strings.HasSuffix(strings.TrimSpace(creds.Auth), "=") {
+		// Modify the decoder to be raw if no padding is present
+		decoder = decoder.WithPadding(base64.NoPadding)
+	}
+
+	base64Decoded, err := decoder.DecodeString(creds.Auth)
+	if err != nil {
+		glog.Warningf("Error Base64 decoding auth field, username/password fields from the registry credentials will be used instead. Error %v", err)
+		return
+	}
+
+	// SplitN required here so that a colon inside the password is not treated as another delimiter
+	splitted := strings.SplitN(string(base64Decoded), ":", 2)
+	if len(splitted) != 2 {
+		glog.Warning("Decoded auth field was not in the format username:password, the username/password fields from the registry credentials will be used instead.")
+		return
+	}
+
+	username = splitted[0]
+	password = splitted[1]
+
+	return
 }
 
 // GetBasicCredentials retrieves username, password from a named secret
