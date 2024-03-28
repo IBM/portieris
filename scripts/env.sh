6c4c8874064bb52db41fe631f987dde0acc0faf7
Stuart Hayton <stuart.hayton@uk.ibm.com>
2024-03-28 14:54:28 +0000

    prepare release
    Signed-off-by: Stuart Hayton <stuart.hayton@uk.ibm.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,11 +2,9 @@
 export PORTIERIS_PULL_APIKEY=
 export PORTIERIS_TESTIMAGE_APIKEY=
 # charts to test
-export VERSION=v0.13.2
-# image tag to test e.g. prep-v0.13.2
+export VERSION=v0.13.13
+# image tag to test e.g. prep-v0.13.13
 export TAG=
-# uuid of the account holding the VA exemption
-export E2E_ACCOUNT_HEADER=
 
 # name of the secret used to pull portieris made from $REG and $PORTIERIS_PULL_APIKEY
 export PULLSECRET=portieris-test 
